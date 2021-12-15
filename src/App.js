f97694363495241fa5a406116914d1caa78d22ab
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-12-15 11:41:37 -0500

    connected to Alan AI to use voice assistant

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
+
+const alanKey = '99e83ca7425c523ed9b0ebb2e0621a152e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if(command  === 'testCommand') {
+          alert('This code was executed');
+        }
+      }
+    })
+  }, [])
   return (
     <div>
       <h1>AIVAN APP</h1>
