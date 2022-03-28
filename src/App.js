5820da6f67bcba494cb2dd369a0f4a0f4c39afe5
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-03-27 23:27:42 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
-import backgroundVideo from './video/video.mp4';
+// import backgroundVideo from './video/video.mp4';
+import useStyles from './styles.js';
 
 
 const alanKey = '99e83ca7425c523ed9b0ebb2e0621a152e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
@@ -20,14 +22,16 @@ const App = () => {
   }, [])
   return (
     <div>
-      <h1>AIVAN APP</h1>
-      <video loop muted autoPlay id='video'>
+      <div className={classes.logoContainer}>
+      <img src="https://images.app.goo.gl/HdgiZfrarsqqF2Eq8" className={classes.alanLogo} alt="logo" />
+      </div>
+      {/* <video loop muted autoPlay id='video'>
         <source
           src={backgroundVideo}
           type="video/mp4"
         />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
       <NewsCards articles={newsArticles} />
     </div>
   );
