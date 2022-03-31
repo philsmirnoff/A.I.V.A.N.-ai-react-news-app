85536fde61db885d8db37991d7bf8e5291304425
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-03-31 00:28:18 -0400

    integrated smooth scrolling and go back capabilities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
 import NewsCards from './components/NewsCards/NewsCards';
 // import backgroundVideo from './video/video.mp4';
 import useStyles from './styles.js';
@@ -14,14 +15,24 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if(command  === 'newHeadlines') {
           setNewsArticles(articles);
           setActiveArticle(-1)
         } else  if (command === 'highlight') {
           setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if(parsedNumber > 20) {
+            alanBtn().playText('Please try that again...');
+          } else if(article) {
+          window.open(article.url, '_blank');
+          alanBtn().playText('Opening...');
         }
       }
+    }
     })
   }, [])
   return (
